23a8a93f81ff819818019b497a296e575b53dc55
Thomas <thomasbarchager@hotmail.dk>
2018-05-03 21:06:15 +0200

    goddammit

--- Menu.java
+++ Menu.java
@@ -118,7 +118,7 @@ public class Menu{
                
             case 4:
                System.out.println("View member");
-               Member.viewMember();
+               System.out.println(Member.viewMember());
                MenuCase();
                break;
          
